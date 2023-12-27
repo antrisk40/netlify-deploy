56d2465847c7cbd2de6965fe1cb58773592739dc
antrisk40 <141990836+antrisk40@users.noreply.github.com>
2023-12-27 09:22:36 +0530

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,13 +3,17 @@ import Image from "next/image";
 
 const Avatar = () => {
   return (
-    <div className="w-full h-full max-w-[737px] max-h-[678px] relative">
+    <div className="hidden xl:block w-full h-full max-w-[737px] max-h-[678px] relative">
       <div className="aspect-[737/678] max-h-full w-full">
-        <Image src={'/avatar.png'} layout="fill" objectFit="contain" alt="" />
+        <Image
+          src={"/avatar.png"}
+          layout="fill"
+          objectFit="contain"
+          alt="Avatar image"
+        />
       </div>
     </div>
   );
 };
 
 export default Avatar;
-
