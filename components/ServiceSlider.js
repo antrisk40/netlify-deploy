9aae4b10f341f3edfccf21fa9acc8bdba6e1ff6e
antrisk40 <141990836+antrisk40@users.noreply.github.com>
2023-12-27 09:12:22 +0530

    Update ServiceSlider.js

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,29 +1,35 @@
-// data
+import {
+  FaCrop,
+  FaPencil2,
+  FaDesktop,
+  FaReader,
+  FaRocket,
+} from "react-icons/fa";
 const serviceData = [
   {
-    icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    icon: <FaCrop />,
+    title: "Branding",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
-    icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    icon: <FaPencil2 />,
+    title: "Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
-    icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    icon: <FaDesktop />,
+    title: "Development",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
-    icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    icon: <FaReader />,
+    title: "Copywriting",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
-    icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    icon: <FaRocket />,
+    title: "SEO",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
 ];
 
