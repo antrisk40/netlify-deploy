0077dc26fa0f02bfd697d683320c6140f5d9211c
antrisk40 <141990836+antrisk40@users.noreply.github.com>
2023-12-27 09:25:17 +0530

    Update ProjectsBtn.js

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,17 +1,32 @@
 //next image
 import Image from "next/image";
-//next link
-import Link from "next/link";
-//icons
+import { useRouter } from "next/router";
 import { HiArrowRight } from "react-icons/hi2";
 
 const ProjectsBtn = () => {
-  return <div className="mx-auto xl:mx-0 ">
-    <Link href={'/work'} className="relative w-[185px] h-[185px] flex justify-center  items-center bg-circleStar bg-cover bg-center bg-no-repeat group">
-      <Image src={'/rounded-text.png'} width={141} height={148} alt= '' className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"/>
-      <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 trasition-all duration-300"/>
-    </Link>
-  </div>;
+  const router = useRouter();
+
+  const handleButtonClick = () => {
+    router.push("/work");
+  };
+
+  return (
+    <div
+      className="mx-auto xl:mx-0 relative z-50 cursor-pointer"
+      onClick={handleButtonClick}
+    >
+      <div className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group">
+        <Image
+          src="/rounded-text.png"
+          width={141}
+          height={148}
+          alt=""
+          className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+        />
+        <HiArrowRight className="absolute text-4xl transition-all duration-300 transform translate-x-0 group-hover:translate-x-2" />
+      </div>
+    </div>
+  );
 };
 
 export default ProjectsBtn;
